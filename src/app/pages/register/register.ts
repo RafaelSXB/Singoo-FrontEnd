fd688be92cfaffbdf0eb4035c2e3c31cc4d8d7dd
unknown <rafael.bento1522@gmail.com>
2026-03-14 19:38:08 -0300

    Construção das validações do formulário - RegisterComponet[UC01]

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+
+  @Output() closed = new EventEmitter<void>();
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const email = this.registerForm.get('email')?.value;
+      const password = this.registerForm.get('password')?.value;
+      console.log('Email:', email);
+      console.log('Password:', password);
+      this.closed.emit();
+    } else {
+      console.log('Form is invalid');
+      this.registerForm.markAllAsTouched();
+    }
+
+  }
+
   passwordVisible: boolean = false;
 
   togglePasswordVisibility() {
